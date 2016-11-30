65020bd85b216f79c03f13a8314741c4bb3c4027
Olga Bezkekova <kavuncool@gmail.com>
2016-11-30 13:54:48 +0200

    FizzBuzz exercise implementation with String parsing

--- src/hortsman_exersices/Test.java
+++ src/hortsman_exersices/Test.java
@@ -5,25 +5,42 @@ package hortsman_exersices;
  */
 public class Test {
     public static void main(String[] args) {
-        /**
-         String fizz = "Fizz";//%3
-         String buzz = "Buzz";//%5
-         String sum = "FizzBuzz";//%15
-         */
+
+        //1st approach
+        String fizz = "Fizz";//%3
+        String buzz = "Buzz";//%5
+        String sum = "FizzBuzz";//%15
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 5 != 0) {
+                System.out.println(fizz);
+            }
+            if (i % 5 == 0 && i % 3 != 0) {
+                System.out.println(buzz);
+            }
+            if (i % 15 == 0) {
+                System.out.println(sum);
+            } else {
+                System.out.println(i);
+            }
+        }
+        //2d approach
         for (int i = 1; i < 101; i++) {
 
             String n = "" + i, f = "Fizz", b = "Buzz";
             //n = Integer.toString(i);
 
             if (i % 3 != 0) {
-                f = "";//not true
+                f = "";
             } else n = "";
             if (i % 5 != 0) {
-                b = "";//not true
+                b = "";
             } else {
                 n = "";
             }
             System.out.println(n + f + b);
         }
+
     }
 }
